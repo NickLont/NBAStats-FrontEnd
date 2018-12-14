0ad18d4bc158ca91046e401bf636b07c79ac4fb8
nicklontorfos <nlontorfos@hotmail.com>
2018-12-14 15:20:59 +0100

    routing working

--- app/containers/navigation/index.js
+++ app/containers/navigation/index.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
 
 class Navbar extends Component {
-  static defaultProps = {}
-
-  static propTypes = {}
-
-  state = {}
-
   render() {
     return (
       <nav role="navigation">
         <ul className="navbar">
-          <li className="navbar__item">Item 1</li>
-          <li className="navbar__item">Item 2</li>
-          <li className="navbar__item">Item 3</li>
+          <li className="navbar__item">
+            <NavLink
+              exact
+              to='/'
+              className="navbar__link"
+              activeClassName="navbar__link--active"
+            >
+              Item 1
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink
+              to='/login'
+              className="navbar__link"
+              activeClassName="navbar__link--active"
+            >
+              Item 2
+            </NavLink>
+          </li>
+          <li className="navbar__link">Item 3</li>
         </ul>
       </nav>
     )
--- app/index.js
+++ app/index.js
@@ -3,24 +3,35 @@ import { Provider } from 'react-redux'
 import configureStore from './store'
 import ReactDOM from "react-dom"
 import "assets/sass/style.scss"
-import { HomePage } from 'containers'
-import { Navbar } from "./containers"
+import { HomePage, Navbar, LoginPage } from 'containers'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+const store = configureStore()
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <BrowserRouter>
+    <>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+        </Switch>
+    </>
+    </BrowserRouter>
+  </Provider>
+)
+Root.propTypes = {
+  store: PropTypes.object.isRequired
+}
 
 class App extends Component {
   render() {
     return (
-      <>
-        <Navbar />
-        <HomePage />
-      </>
+      <Root store={store} />
     )
   }
 }
-const store = configureStore()
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("app")
-)
+ReactDOM.render(<App />, document.getElementById("app"))
